a6c2afd7197a3bdd9943c2d97b08b6982f69cf9d
Annchous <annchooousss@gmail.com>
2021-04-05 03:48:40 +0300

    feat: null literal expressions analysis

--- Source/NullRefChecksAnalyzer/NullRefChecksAnalyzer/NullRefChecksAnalyzer/NullRefChecksAnalyzer.cs
+++ Source/NullRefChecksAnalyzer/NullRefChecksAnalyzer/NullRefChecksAnalyzer/NullRefChecksAnalyzer.cs
@@ -18,9 +18,9 @@ namespace NullRefChecksAnalyzer
         // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
         // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Localizing%20Analyzers.md for more on localization
         private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
-        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MessageFormat = "Redundant null reference check";
         private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
-        private const string Category = "Naming";
+        private const string Category = "Syntax";
 
         private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
 
@@ -33,22 +33,27 @@ namespace NullRefChecksAnalyzer
 
             // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
             // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
-            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
+            context.RegisterSyntaxNodeAction(AnalyzeNullChecks, SyntaxKind.MethodDeclaration);
         }
 
-        private static void AnalyzeSymbol(SymbolAnalysisContext context)
+        private static void AnalyzeNullChecks(SyntaxNodeAnalysisContext context)
         {
-            // TODO: Replace the following code with your own analysis, generating Diagnostic objects for any issues you find
-            var namedTypeSymbol = (INamedTypeSymbol)context.Symbol;
-
-            // Find just those named type symbols with names containing lowercase letters.
-            if (namedTypeSymbol.Name.ToCharArray().Any(char.IsLower))
+            var semanticModel = context.SemanticModel;
+            var syntaxTree = semanticModel.SyntaxTree;
+            var methodDeclaration = (MethodDeclarationSyntax) context.Node;
+            foreach (var ifStatement in methodDeclaration.DescendantNodes().OfType<IfStatementSyntax>())
             {
-                // For all such symbols, produce a diagnostic.
-                var diagnostic = Diagnostic.Create(Rule, namedTypeSymbol.Locations[0], namedTypeSymbol.Name);
-
-                context.ReportDiagnostic(diagnostic);
+                var literalExpressions = ifStatement.Condition.DescendantNodes().OfType<LiteralExpressionSyntax>().ToList();
+                if (literalExpressions.Any())
+                {
+                    var nullLiteralExpressions = GetNullLiteralExpressions(literalExpressions);
+                    nullLiteralExpressions.ForEach(literalExpression => context.ReportDiagnostic(Diagnostic.Create(Rule, literalExpression.Parent.GetLocation(), "Redundant null reference check.")));
+                }
             }
         }
+
+        private static List<LiteralExpressionSyntax>
+            GetNullLiteralExpressions(List<LiteralExpressionSyntax> literalExpressions) => literalExpressions
+            .Where(literalExpression => literalExpression.Kind() is SyntaxKind.NullLiteralExpression).ToList();
     }
 }
